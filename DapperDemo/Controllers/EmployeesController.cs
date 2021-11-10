70379436cb6134385e5d7eeb1dd2240e285a1507
JPThakur361 <js1081996@gmail.com>
2021-11-10 15:55:10 +0530

    Add project files.

---
+++ DapperDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DapperDemo.Data;
+using DapperDemo.Models;
+using DapperDemo.Repository;
+
+namespace DapperDemo.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        //   private readonly ApplicationDbContext _context;
+        private readonly ICompanyRepository _compRepo;
+        private readonly IEmployeeRepository _empRepo;
+
+
+
+        public EmployeesController(ICompanyRepository compRepo , IEmployeeRepository empRepo)
+        {
+            _compRepo = compRepo;
+            _empRepo = empRepo; 
+
+         
+        }
+
+        [BindProperty]
+        public Employee Employee { get; set; }
+        // GET: Companies
+        public async Task<IActionResult> Index()
+        {
+            return View(_empRepo.GetAll()) ;
+        }
+
+        // GET: Companies/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var company = _compRepo.find(id.GetValueOrDefault());
+
+            
+        //    if (company == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(company);
+        //}
+
+        // GET: Companies/Create
+        public IActionResult Create()
+        {
+           IEnumerable<SelectListItem> companyList = _compRepo.GetAll().Select(i => new SelectListItem
+            {
+               Text = i.Name,
+                Value = i.CompanyId.ToString()
+            }) ;
+            ViewBag.CompanyList = companyList;
+
+            return View();
+        }
+
+        // POST: Companies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Create")]
+        public async Task<IActionResult> CreatePOST()
+        {
+            if (ModelState.IsValid)
+            {
+                _empRepo.Add(Employee);
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Employee);
+        }
+
+        // GET: Companies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Employee = _empRepo.find(id.GetValueOrDefault());
+
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+            return View(Employee);
+        }
+
+        // POST: Companies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id != Employee.EmployeeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+
+                _empRepo.Update(Employee);
+                 
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Employee);
+        }
+
+        // GET: Companies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            _empRepo.Remove(id.GetValueOrDefault());
+            return RedirectToAction(nameof(Index));
+
+        }
+
+    
+       
+      
+    }
+}
---
+++ DapperDemo/Repository/CompanyRepositoryEF.cs
@@ -0,0 +1,68 @@
+﻿using DapperDemo.Data;
+using DapperDemo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperDemo.Repository
+{
+    public class CompanyRepositoryEF : ICompanyRepository
+
+    {
+        //private  readonly IDbConnection db;
+        private readonly ApplicationDbContext _db;
+
+        //public CompanyRepository(IConfiguration configuration )
+        //{
+        //    this.db = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+
+        //}
+
+        public CompanyRepositoryEF(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Company Add(Company company)
+        {
+            //var sql = "INSERT INTO Companies (Name, Address, City, State, PostalCode) VALUES(@Name, @Address, @City, @State, @PostalCode);"
+            //                + "SELECT CAST(SCOPE_IDENTITY() as int); ";
+
+
+            //var id = db.Query<int>(sql, company).Single();
+            _db.Companies.Add(company);
+            _db.SaveChanges();
+            return company;
+        }
+
+        public Company find(int id)
+        {
+            return _db.Companies.FirstOrDefault(u => u.CompanyId == id);
+
+        }
+
+        public List<Company> GetAll()
+        {
+            return _db.Companies.ToList();
+        }
+
+        public void Remove(int id)
+        {
+            Company company = _db.Companies.FirstOrDefault(u => u.CompanyId == id);
+            _db.Companies.Remove(company);
+                _db.SaveChanges();
+            return;
+
+        }
+
+        public Company Update(Company company)
+        {
+            _db.Companies.Update(company);
+            _db.SaveChanges();
+            return company;
+        }
+    }
+}
